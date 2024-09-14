dcf37ac4b759e54c3d2e0b0025e66973d2022ed2
Skonst07 <124036028+Skonst07@users.noreply.github.com>
2024-09-14 15:22:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ const hidden = document.getElementById("hidden");
 
 let count = 0;
 
-reset.onclick = function(){
-    count = 0;
-    counter.textContent = 0
-    hidden.style.display = "none";
-}
-
-increase.onclick = function(){
+function updateCounter() {
     count++;
     counter.textContent = count;
 
-     if (count === 100) {
+    if (count >= 100) {
         hidden.style.display = "block";
     }
 }
+
+function resetCounter() {
+    count = 0;
+    counter.textContent = count;
+    hidden.style.display = "none";
+}
+
+// Mouse click events
+increase.addEventListener('click', updateCounter);
+reset.addEventListener('click', resetCounter);
+
+// Touch events
+increase.addEventListener('touchend', (event) => {
+    event.preventDefault(); // Prevents any default action for touch
+    updateCounter();
+});
+
+reset.addEventListener('touchend', (event) => {
+    event.preventDefault(); // Prevents any default action for touch
+    resetCounter();
+});
